c439a5781cffc2927eadd5f45c7404e5c6d6bec9
ph <dinhhphong2909@gmail.com>
2026-05-13 14:46:37 +0700

    Updated

--- Lab 2/lab2_assignment/assignment_3.py
+++ Lab 2/lab2_assignment/assignment_3.py
@@ -9,7 +9,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report
 import matplotlib.pyplot as plt
 
-print("--- 1. Logistic Regression (Binary: 0 vs 1) ---")
+print("1. Logistic Regression (Binary: 0 vs 1)")
 X_bin, y_bin = load_mnist_ubyte(0, 1)
 
 log_reg = LogisticRegression(solver='lbfgs', max_iter=200)
@@ -18,7 +18,7 @@ log_reg.fit(X_bin, y_bin)
 y_pred_bin = log_reg.predict(X_bin)
 print(f"Accuracy (Binary 0 vs 1): {accuracy_score(y_bin, y_pred_bin)*100:.2f}%")
 
-print("\n--- 2. Softmax Regression (Full 10 classes) ---")
+print("\n2. Softmax Regression (Full 10 classes)")
 X_all, y_all = load_mnist_full()
 
 softmax_reg = LogisticRegression(solver='lbfgs', max_iter=500)
